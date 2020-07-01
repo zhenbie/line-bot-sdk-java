9711bba7855376e2e445a7d09ec6a302214fef65
zhenbaolei <zhenbaolei@jd.com>
2020-07-01 14:49:04 +0800

    init

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -21,16 +21,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+//import com.linecorp.bot.model.event.Event;
+//import com.linecorp.bot.model.event.MessageEvent;
+//import com.linecorp.bot.model.event.message.TextMessageContent;
+//import com.linecorp.bot.model.message.Message;
+//import com.linecorp.bot.model.message.TextMessage;
+//import com.linecorp.bot.spring.boot.annotation.EventMapping;
+//import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @SpringBootApplication
-@LineMessageHandler
+//@LineMessageHandler
 public class EchoApplication {
     private final Logger log = LoggerFactory.getLogger(EchoApplication.class);
 
@@ -38,15 +38,15 @@ public class EchoApplication {
         SpringApplication.run(EchoApplication.class, args);
     }
 
-    @EventMapping
-    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        log.info("接收到消息事件TextMessageContent: " + event);
-        final String originalMessageText = event.getMessage().getText();
-        return new TextMessage(originalMessageText);
-    }
-
-    @EventMapping
-    public void handleDefaultMessageEvent(Event event) {
-        System.out.println("event: " + event);
-    }
+//    @EventMapping
+//    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+//        log.info("接收到消息事件TextMessageContent: " + event);
+//        final String originalMessageText = event.getMessage().getText();
+//        return new TextMessage(originalMessageText);
+//    }
+//
+//    @EventMapping
+//    public void handleDefaultMessageEvent(Event event) {
+//        System.out.println("event: " + event);
+//    }
 }
