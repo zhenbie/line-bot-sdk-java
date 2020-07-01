e88055e3bbb428596282d0c12000ff5dbee3c32f
zhenbaolei <zhenbaolei@jd.com>
2020-07-01 16:29:46 +0800

    init

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/controller/EchoSampleController.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/controller/EchoSampleController.java
@@ -16,20 +16,38 @@
 
 package com.example.bot.spring.echo.controller;
 
+import static java.util.Collections.singletonList;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.linecorp.bot.client.LineBlobClient;
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @LineMessageHandler
 public class EchoSampleController {
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
+    @Autowired
+    private LineBlobClient lineBlobClient;
 
     @EventMapping
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
@@ -41,10 +59,38 @@ public class EchoSampleController {
     @EventMapping
     public void handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
         log.info("接收到贴图消息事件StickerMessageContent: " + event);
+        handleSticker(event.getReplyToken(), event.getMessage());
     }
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull Message message) {
+        reply(replyToken, singletonList(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+        reply(replyToken, messages, false);
+    }
+
+    private void reply(@NonNull String replyToken,
+                       @NonNull List<Message> messages,
+                       boolean notificationDisabled) {
+        try {
+            BotApiResponse apiResponse = lineMessagingClient
+                    .replyMessage(new ReplyMessage(replyToken, messages, notificationDisabled))
+                    .get();
+            log.info("Sent messages: {}", apiResponse);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void handleSticker(String replyToken, StickerMessageContent content) {
+        reply(replyToken, new StickerMessage(
+                content.getPackageId(), content.getStickerId())
+        );
     }
 }
